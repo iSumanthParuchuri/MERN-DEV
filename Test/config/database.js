d4d0bbfe02ce4d23e03e4600f7eecfb5bf38aea2
iSumanthParuchuri <iamlucifer2hell@gmail.com>
2021-03-15 10:40:57 +0530

    Connected to MongoDB

---
+++ Test/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+dotenv.config({path : './config.env'});
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }).then(con => {
+          console.log(`MongoDB connected with HOST : ${process.env.DB_LOCAL_URL}`);
+      });
+}
+
+module.exports = connectDatabase;
--- Test/index.js
+++ Test/index.js
@@ -2,7 +2,12 @@ const app = require('./app');
 
 const dotenv = require('dotenv');
 
+const connectDatabase = require('./config/database');
+
 dotenv.config({path : './config/config.env'});
+
+//Connecting to mongoDB database
+connectDatabase();
 
 app.listen(process.env.PORT,() => {
     console.log(`Listening on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
