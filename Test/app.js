a418cfcef71a0fed3a9f7584ca8c4bf28ed8839e
iSumanthParuchuri <iamlucifer2hell@gmail.com>
2021-03-22 21:50:02 +0530

    Added RequestLogger

--- Test/app.js
+++ Test/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 //const router = express.Router();
 const route = require('./Routes/routing');
+const myReqLogger = require('./utilities/RequestLogger');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(myReqLogger);
 app.use('/',route);
 
 module.exports = app;
