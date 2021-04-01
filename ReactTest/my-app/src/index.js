e14b8fcab2283d3ecd14183f859c5fc68859488f
iSumanthParuchuri <iamlucifer2hell@gmail.com>
2021-04-01 11:44:49 +0530

    Test State usage

--- ReactTest/my-app/src/index.js
+++ ReactTest/my-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-import App from './App';
+//import App from './App';
+import App1 from './App1';
 //import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 //serviceWorker.unregister();
+ReactDOM.render(<App1 />, document.getElementById('root'));
