98f1b9c7896f5f88eaefc8b2cc04894f273fb332
iSumanthParuchuri <iamlucifer2hell@gmail.com>
2021-04-13 10:14:55 +0530

    Added routes 1.0

--- ReactTest/my-app/src/index.js
+++ ReactTest/my-app/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-//import App from './App';
-//import App1 from './App1';
-//import Timer from './Timer';
-import App2 from './App2';
+import Header from './components/Header';
 //import * as serviceWorker from './serviceWorker';
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header/>, document.getElementById('root'));
 //serviceWorker.unregister();
-//ReactDOM.render(<App1 />, document.getElementById('root'));
-//ReactDOM.render(<Timer />, document.getElementById('root'));
-ReactDOM.render(<App2 />, document.getElementById('root'));
